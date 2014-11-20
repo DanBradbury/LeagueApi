5fb28d14eecaf1cedd446f8e65943acd389f7e2a
Michael Nunez <mnunez903@gmail.com>
2014-11-20 11:50:22 -0500

    4.20.1 update added new champ (Kalista)

--- spec/champion_spec.rb
+++ spec/champion_spec.rb
@@ -3,7 +3,7 @@ require 'pry'
 
 describe LeagueApi::Champion do
   before :all do
-    CHAMP_NUM = 121 #As of Patch 4.18.1
+    CHAMP_NUM = 122 #As of Patch 4.20.1
 		LeagueApi.set_region "na"
   end
 	it 'should return correct champion list' do
